a9384ff025ddd986931879aec8935884051f83ea
unknown <jtuyen99@gmail.com>
2020-11-08 00:03:31 -0800

    Completed main project.

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 var glasses = 0;
 var lastClickTime = 0;
+var quotes = [
+    '\"Even the heaviest door can be open.\" -Braum',
+    '\"Fear is the first of many foes.\" -Garen',
+    '\"If you hit a wall, hit it HARD!\" -Vi',
+    '\"Stay Positive!\" -Lux',
+    '\"The climb may be long, but the view is worth it.\" -Taric',
+    '\"Even the smallest stone can start an avalanche.\" -Taliyah',
+    '\"Every step is one step closer.\" -Poppy',
+    '\"It\'s ok to be sad sometimes, that\'s how you know when you\'ll be happy!\" -Nunu',
+    '\"Always trust in your spirit.\" -Karma',
+    '\"It\'s not about how much time you have, it\'s how you use it.\" -Ekko',
+    '\"All life is precious, yes... even yours.\" -Ivern',
+    '\"No one said this would be easy.\" -Senna'
+]
+
+function getQuote(){
+    var randomNumber = Math.floor(Math.random() * (quotes.length));
+    document.getElementById("quotes").innerHTML = quotes[randomNumber];
+};
 
 function resetButton(){
+    if(glasses != 0){
+        getQuote();
+    }
     glasses = 0;
     document.getElementById("glasses").innerHTML = glasses;
 };
@@ -10,6 +32,7 @@ function subButton(){
     if(glasses >= 1){
         glasses = glasses - 1;
         document.getElementById("glasses").innerHTML = glasses;
+        getQuote();
     }
 };
 
@@ -20,6 +43,7 @@ function addButton(){
         alert("Liquids are good for you, but drinking too fast isn't.");
     }
     lastClickTime =  new Date();
+    getQuote();
 };
 
 function reminderDrink(){
@@ -27,7 +51,3 @@ function reminderDrink(){
 }
 
 setInterval(reminderDrink, 600000)
-
-var quotes = [
-    
-]
